03cba6f91b5a5f9d1652c7b574d4416968e90eff
JoepMulder <Joep.Mulder@hotmail.com>
2016-12-22 23:20:35 +0100

    SeedData update

--- HoneymoonShop/src/HoneymoonShop/Models/CatalogusFilterModel.cs
+++ HoneymoonShop/src/HoneymoonShop/Models/CatalogusFilterModel.cs
@@ -10,6 +10,15 @@ namespace HoneymoonShop.Models
     public class CatalogusFilterModel
     {        
         public List<int> selectedMerken { get; set; }
+        public List<int> selectedStijlen { get; set; }
+
+        public int selectedMinimumPrijs { get; set; }
+        public int selectedMaximumPrijs { get; set; }
+
+        public List<int> selectedNeklijnen { get; set; }
+        public List<int> selectedSilhouetten { get; set; }
+        public List<int> selectedKleuren { get; set; }
+
 
         public List<Jurk> filteredJurken { get; set; }
     }
