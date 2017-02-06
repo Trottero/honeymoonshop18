4ee7dc5c1342a4277c5a0ff7704db107eeaf773f
MrRaines <15011097@student.hhs.nl>
2017-02-06 14:13:10 +0100

    migration toegevoegd

---
+++ HoneymoonShop/src/HoneymoonShop/Migrations/20170206131128_[initt].cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HoneymoonShop.Migrations
+{
+    public partial class initt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Trouwdatum",
+                table: "Klanten2",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Datum",
+                table: "Afspraken2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Trouwdatum",
+                table: "Klanten2",
+                nullable: false);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Datum",
+                table: "Afspraken2",
+                nullable: false);
+        }
+    }
+}
